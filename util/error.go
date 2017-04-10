63a7393dd635393cb508c50f2bbe4fe316c2d407
Matheus Salmi <mathsalmi@gmail.com>
2017-04-10 13:26:07 -0300

    Add comments on exported items of pkg util

--- util/error.go
+++ util/error.go
@@ -2,18 +2,22 @@ package util
 
 import "bytes"
 
+// AppError represents an error
 type AppError struct {
 	errors []error
 }
 
+// NewError returns a pointer of error
 func NewError() *AppError {
 	return &AppError{}
 }
 
+// Append appends a new error to the list of errors
 func (e *AppError) Append(err error) {
 	e.errors = append(e.errors, err)
 }
 
+// Error returns all errors in string
 func (e *AppError) Error() string {
 	b := bytes.Buffer{}
 
