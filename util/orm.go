8341b1a04d2486231413ea05142a8139c0dd8008
Matheus Salmi <mathsalmi@gmail.com>
2017-04-10 23:19:10 -0300

    Rename XORM instance var

--- util/orm.go
+++ util/orm.go
@@ -2,15 +2,15 @@ package util
 
 import "github.com/go-xorm/xorm"
 
-// instance holds an instance of the XORM
-var instance *xorm.Engine
+// dbinstance holds an dbinstance of the XORM
+var dbinstance *xorm.Engine
 
 // SetDb sets an instance of the XORM in the internal guard
 func SetDb(orm *xorm.Engine) {
-	instance = orm
+	dbinstance = orm
 }
 
 // GetDb returns the XORM instance
 func GetDb() *xorm.Engine {
-	return instance
+	return dbinstance
 }
